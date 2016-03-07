c33c38959ae2b3c64c0810f0e304bbd69cd64bef
Ivan Bozhanov <git@vakata.com>
2016-03-07 18:19:44 +0200

    removed IP and UA methods

--- src/Request.php
+++ src/Request.php
@@ -140,50 +140,7 @@ class Request extends Message implements RequestInterface
         $req->removeHeader('Transfer-Encoding');
         return $req;
     }
-    /**
-     * get the client's IP address
-     * @method ipAddressFromRequest
-     * @return string               the client's IP
-     * @codeCoverageIgnore
-     */
-    public static function ipAddressFromRequest() {
-        $ip = '0.0.0.0';
-        // TODO: check if remote_addr is a cloudflare one and only then read the connecting ip
-        // https://www.cloudflare.com/ips-v4
-        // https://www.cloudflare.com/ips-v6
-        if (false && isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
-            $ip = $_SERVER["HTTP_CF_CONNECTING_IP"];
-        }
-        elseif (isset($_SERVER['REMOTE_ADDR']) && isset($_SERVER['HTTP_CLIENT_IP'])) {
-            $ip = $_SERVER['HTTP_CLIENT_IP'];
-        }
-        elseif (isset($_SERVER['REMOTE_ADDR'])) {
-            $ip = $_SERVER['REMOTE_ADDR'];
-        }
-        elseif (isset($_SERVER['HTTP_CLIENT_IP'])) {
-            $ip = $_SERVER['HTTP_CLIENT_IP'];
-        }
-        elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-        }
-        if (strpos($ip, ',') !== false) {
-            $ip = @end(explode(',', $ip));
-        }
-        $ip = trim($ip);
-        if (false === ($ip = filter_var($ip, FILTER_VALIDATE_IP))) {
-            $ip = '0.0.0.0';
-        }
-        return $ip;
-    }
-    /**
-     * Get the user agent from the request.
-     * @method userAgentFromRequest
-     * @return string               the user agent
-     * @codeCoverageIgnore
-     */
-    public static function userAgentFromRequest() {
-        return isset($_SERVER) && isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '';
-    }
+    
 
     /**
      * get the HTTP verb used (GET / POST / PUT / etc), defaults to `GET`
